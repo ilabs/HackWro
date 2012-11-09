2c27378b6d38045f7b928bd1844f812a69273436
Paweł Nużka <pawelqus@gmail.com>
2012-11-09 21:53:44 +0100

    initial commit

---
+++ HackWro/HackWro/HWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HWAppDelegate.h
+//  HackWro
+//
+//  Created by Paweł Nużka on 09.11.2012.
+//  Copyright (c) 2012 ploomworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HackWro/HackWro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HackWro
+//
+//  Created by Paweł Nużka on 09.11.2012.
+//  Copyright (c) 2012 ploomworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HWAppDelegate class]));
+    }
+}
